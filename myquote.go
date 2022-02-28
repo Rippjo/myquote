571bbe685f6f83dc2534fd599bffb76aeb694d58
Rippjo <Kasperforum@hotmail.no>
2022-02-28 17:16:35 +0100

    Yo

---
+++ myquote.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+    "fmt"
+    "github.com/Rippjo/myquote"
+)
+func main() {
+    fmt.Println(del2pakke.TestQuote())
+}
---
+++ package/pakke.go
@@ -0,0 +1,10 @@
+package del2pakke
+
+import (
+    "rsc.io/quote"
+)
+
+func TestQuote() string {
+    return quote.Glass()+ "\n" + quote.Go()+ "\n" + quote.Hello()+ "\n" + quote.Opt();
+
+}
